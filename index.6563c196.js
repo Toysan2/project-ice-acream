e79b5b4dd0445de59c80e7ec964d484754329b62
Toysan2 <51274132+Toysan2@users.noreply.github.com>
2023-07-10 18:43:50 +0000

    Deploying to gh-pages from @ Toysan2/project-ice-cream@86e59c6d58bcba99c57907089fa35c236118a471 🚀

---
+++ index.6563c196.js
@@ -0,0 +1,2 @@
+new Swiper(".photo-slider .swiper-container",{effect:"fade",slidesPerView:1,loop:!0,autoplay:{delay:3e3},speed:2e3});new Swiper(".opinion-slider",{slidesPerView:1,direction:"horizontal",loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.6563c196.js.map
